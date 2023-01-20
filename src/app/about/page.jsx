15def94f676375fb610ee591d5be56e215ff38a0
dbarkowsky <dylanbarkowsky@gmail.com>
2023-01-20 12:24:01 -0800

    testing grid

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,21 @@
-const About = () => <p>About</p>
+"use client"
+
+import Grid from '@mui/material/Unstable_Grid2';
+
+const About = () => {
+    return (
+        <Grid container spacing={2} padding="2em">
+            <Grid xs={12}>
+                Testing
+            </Grid>
+            <Grid xs={8}>
+                Left
+            </Grid>
+            <Grid xs={4}>
+                Right
+            </Grid>
+        </Grid>
+    );
+}
 
 export default About;
