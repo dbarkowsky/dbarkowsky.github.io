6903bb51844689fd29dad0a09eaf6f62dacb855c
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-01-23 14:02:32 -0800

    Clickable surface on cards.

--- src/components/posts/PostBlock.jsx
+++ src/components/posts/PostBlock.jsx
@@ -2,13 +2,15 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 import Button from '@mui/material/Button';
 import colours from '../Colours';
 
 const PostBlock = ({title, date, blurb, paths}) => {
     const cardButtonStyle = {
         color: colours.lightText,
-        margin: '0 0.5em',      
+        margin: '0 auto',
+        width: '100%',  
         '&:hover': {
             color: colours.darkText,
             backgroundColor: colours.lightBackground
@@ -17,17 +19,19 @@ const PostBlock = ({title, date, blurb, paths}) => {
 
     return (
     <Card sx={{ maxWidth: 345, backgroundColor: colours.darkBackground, color: colours.lightText }}>
-        <CardMedia
-            component="img"
-            alt="green iguana"
-            height="140"
-            image={paths.img}
-        />
-        <CardContent>
-            <h3 style={{ margin: '0'}}>{title}</h3>
-            <h5 style={{ marginTop: '0'}}>{date.toLocaleDateString()}</h5>
-            <p style={{ fontSize: '12pt' }}>{blurb}</p>
-        </CardContent>
+        <CardActionArea href={paths.post}>
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={paths.img}
+            />
+            <CardContent>
+                <h3 style={{ margin: '0'}}>{title}</h3>
+                <h5 style={{ marginTop: '0'}}>{date.toLocaleDateString()}</h5>
+                <p style={{ fontSize: '12pt' }}>{blurb}</p>
+            </CardContent>
+        </CardActionArea>
         <CardActions>
             <Button size="small" href={paths.post} sx={cardButtonStyle}>More</Button>
             {paths.code ? <Button size="small" href={paths.code} sx={cardButtonStyle}>Code</Button> : <></>}
