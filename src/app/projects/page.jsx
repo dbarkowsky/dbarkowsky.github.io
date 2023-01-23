502af18faf6373104a06e3fa5f73a6581cc268e7
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-01-23 08:37:58 -0800

    Posts page layout complete

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,25 +7,31 @@ import posts from "@/data/posts";
 import { Divider } from "@mui/material";
 
 const Projects = () => {
-    let post = posts[0];
-
+    const yearList = new Set(posts.map(post => post.date.getFullYear()).sort((a, b) => b - a));
     const noPaddingStyle = { padding: '0' };
+
     return (
         <Grid container spacing={2} padding="1em" sx={{
             backgroundColor: colours.lightBackground,
             margin: 0,
         }}>
-            <Grid xs={12} sx={noPaddingStyle}>
-            <Divider textAlign="left" sx={{ fontSize: '16pt', margin: '1em 0' }}>2023</Divider>
-                <Grid container spacing={2} sx={noPaddingStyle}>
-                    <Grid xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>
-                    <Grid xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>
-                    <Grid xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>
-                    <Grid xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>
-                    <Grid xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>
-                </Grid>
-                
-            </Grid>
+        {
+            Array.from(yearList).map(year => {
+                const yearsPosts = posts
+                    .filter(post => post.date.getFullYear() == year)
+                    .sort((a, b) => b.date - a.date);
+                return (
+                    <Grid key={year} xs={12} sx={noPaddingStyle}>
+                    <Divider textAlign="left" sx={{ fontSize: '16pt', margin: '1em 0' }}>{year}</Divider>
+                        <Grid container spacing={2} sx={noPaddingStyle}>
+                        {
+                            yearsPosts.map(post => <Grid key={post.title} xs={12} sm={6} md={4}><PostBlock {...post}/></Grid>)
+                        }
+                        </Grid>
+                    </Grid>
+                );
+            })
+        }
         </Grid>
     );
 }
--- src/data/posts.jsx
+++ src/data/posts.jsx
@@ -10,6 +10,35 @@ const posts = [
             demo: 'https://mui.com/material-ui/react-card/'
         }
     },
+    {
+        title: 'dfsdf Post',
+        date: new Date('2023-01-01'),
+        blurb: 'Check out this post. It is a tester for the card component.',
+        paths: {
+            img: '/landing/2021-09-18.3.jpg',
+            post: '/about',
+            demo: 'https://mui.com/material-ui/react-card/'
+        }
+    },
+    {
+        title: 'Firerst Post',
+        date: new Date('2023-02-23'),
+        blurb: 'Check out this post. It is a tester for the card component.',
+        paths: {
+            img: '/landing/2021-09-18.3.jpg',
+            post: '/about',
+        }
+    },
+    {
+        title: 'First rewrwe',
+        date: new Date('2022-01-23'),
+        blurb: 'Check out this post. It is a tester for the card component.',
+        paths: {
+            img: '/landing/2021-09-18.3.jpg',
+            post: '/about',
+            code: 'https://stackoverflow.com/questions/36482126/how-to-enlarge-the-svg-icon-in-material-ui-iconbuttons',
+        }
+    },
 ]
 
 export default posts;
