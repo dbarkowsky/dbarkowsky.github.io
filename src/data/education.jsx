031cf1c0ff3415a9cfb7b15f47311b11b78bed59
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-01-24 23:17:08 -0800

    Filled out education

--- src/data/education.jsx
+++ src/data/education.jsx
@@ -5,8 +5,36 @@ const education = [
         certificate: 'Information & Computer Systems Diploma',
         issuer: 'Camosun College',
         date: '2023',
-        blurb: 'This is a blurb',
+        blurb: 'Focused on application development, software design, security, networking, and more.',
         importance: 100
+    },
+    {
+        certificate: 'B.A. (Hons) - Applied Linguistics',
+        issuer: 'University of Victoria',
+        date: '2013',
+        blurb: 'Included classes on education, linguistics, and computer science. Completed my Honours Thesis on Computer Assisted Language Learning.',
+        importance: 100
+    },
+    {
+        certificate: 'Professional Certificate in Asset Management',
+        issuer: 'IPWEA & NAMS Canada',
+        date: '2020',
+        blurb: 'Learning how to track, evaluate, and appraise assets.',
+        importance: 0
+    },
+    {
+        certificate: 'CompTIA A+',
+        issuer: 'CompTIA',
+        date: '2019',
+        blurb: 'Covered the basics of computer systems and hardware.',
+        importance: 0
+    },
+    {
+        certificate: 'ITIL 4 Foundation',
+        issuer: 'Axelos',
+        date: '2019',
+        blurb: 'Explored the ideologies and best practices centred around management of IT-related services.',
+        importance: 0
     },
 ]
 
