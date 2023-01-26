cf72c2c8d59112e98fd107b3f65d028896722fd6
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-01-25 22:25:59 -0800

    renaming folder again

---
+++ src/components/navigation/NavBarLink.jsx
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+
+const NavBarLink = ({href, text}) => {
+    return (
+    <Button 
+        variant="text" 
+        href={href} 
+        sx={{ 
+            flexGrow: 0,
+            color: "white", 
+        }}>
+        <b>{text}</b>
+    </Button>);
+}
+
+export default NavBarLink;
